21f8ecf0b5a7141a1a438f01601f6244e983d0ca
Iader E. Garcia G <iadergg@gmail.com>
2020-02-02 17:42:53 -0500

    Changes in sql query in get_best_students function

--- lib/lib.php
+++ lib/lib.php
@@ -6,28 +6,30 @@ require_once '../../../config.php';
  * get_best_students
  * @param int $courseid    Course ID
  * @param int $n_students  Number of studentes to return
+ * @return stdClass
+ * 
  * @author Iader E. Garcia G. <iadergg@gmail.com>
  */
 function get_best_students($courseid, $n_students) {
 
     global $DB;
 
-    //$best_students_array = array();
-
-    $sql_query  =   "SELECT users.id, users.username, users.firstname, users.lastname, ggrades.finalgrade
+    $sql_query  =   "SELECT 
+                        users.id AS userid, 
+                        ggrades.finalgrade AS finalgrade, 
+                        ROW_NUMBER() OVER (ORDER BY ggrades.finalgrade DESC) AS list_position
                     FROM 
                         {grade_grades} AS ggrades 
                         INNER JOIN {grade_items} AS gitems ON ggrades.itemid = gitems.id
                         INNER JOIN {course} AS courses ON gitems.courseid = courses.id
                         INNER JOIN {user} AS users ON ggrades.userid = users.id
                     WHERE
-                        ggrades.finalgrade 
-                        gitems.itemtype = 'course'
+                        ggrades.finalgrade IS NOT NULL
+                        AND gitems.itemtype = 'course'
                         AND courses.id = $courseid
                         AND users.deleted = 0
                     ORDER BY 
-                        ggrades.finalgrade DESC,
-                        users.username ASC
+                        ggrades.finalgrade DESC
                     LIMIT $n_students";
 
     $best_students_array = $DB->get_records_sql($sql_query);
@@ -35,4 +37,4 @@ function get_best_students($courseid, $n_students) {
     return $best_students_array;
 }
 
-print_r(get_best_students(32542, 100));
+print_r(get_best_students(32542, 3));
